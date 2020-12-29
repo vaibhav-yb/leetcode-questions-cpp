47c86d6d41ade9598ccd7c4cbf48b1d764204de7
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2020-12-29 20:14:03 +0530

    faster than 25.57%, see previous commit for another solution

--- Linked List in Binary Tree.cpp
+++ Linked List in Binary Tree.cpp
@@ -1,11 +1,6 @@
 // 1367. Linked List in Binary Tree
 // Medium.
 
-/*
-  THIS SOLUTION IS GIVING TLE IN A FEW CASES BUT I THINK IT IS CORRECT AND
-  IT NEEDS TO BE OPTIMIZED
-*/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -29,26 +24,24 @@
  */
 class Solution {
 public:
-    bool doesPathExist(TreeNode *root, ListNode *head, bool changed)
+    bool pathAfterMatched(ListNode *head, TreeNode *root)
     {
         if(head == NULL)
             return true;
+        if(root == NULL || head->val != root->val)
+            return false;
         
+        return pathAfterMatched(head->next, root->left) || pathAfterMatched(head->next, root->right);
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(head == NULL)
+            return true;
         if(root == NULL)
             return false;
         
-        if(changed && root->val != head->val)
-            return false;
+        if(pathAfterMatched(head, root))
+            return true;
         
-        bool matched = false, unmatched = false;
-        if(root->val == head->val)
-            matched = doesPathExist(root->left, head->next, true) || doesPathExist(root->right, head->next, true);
-        
-        unmatched = doesPathExist(root->left, head, changed) || doesPathExist(root->right, head, changed);
-        
-        return (matched || unmatched);
-    }
-    bool isSubPath(ListNode* head, TreeNode* root) {
-        return doesPathExist(root, head, false);
+        return isSubPath(head, root->left) || isSubPath(head, root->right);
     }
 };
